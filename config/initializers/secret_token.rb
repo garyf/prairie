c3075b273e26f50021debc7b2bcaba5eee7e25e1
Gary Fleshman <gfleshman@newforge-tech.com>
2013-07-19 17:05:44 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Prairie0::Application.config.secret_key_base = '328744192847711003e2b689af5a9e4042242a0153e1c7cb0a09f43d32c90ea3ffe262d50f2eddf8d6addec2d8a47bb6f3a9ef6facba30ac1b35c8b77ed6a60f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Prairie0::Application.config.session_store :cookie_store, key: '_prairie_0_session'
