7098d8d733212da186125c9db500abf3af75faf7
Gary Fleshman <gfleshman@newforge-tech.com>
2013-07-20 18:34:03 -0400

    field_sets templates

---
+++ app/decorators/person_field_set_decorator.rb
@@ -0,0 +1,11 @@
+# coding: utf-8
+module PersonFieldSetDecorator
+
+  def name_with_type
+    "#{name}, for people"
+  end
+
+  def type_human
+    'People'
+  end
+end
--- app/views/field_sets/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @field_set, :type, :name, :description, :created_at, :updated_at
