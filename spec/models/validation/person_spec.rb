bff268f5d9ba1770529d9ef7f4a0b9c8bfb0d50e
Gary Fleshman <gfleshman@newforge-tech.com>
2013-07-20 15:34:46 -0400

    spec validation person email

--- spec/models/validation/person_spec.rb
+++ spec/models/validation/person_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 describe Person do
   context '::validates w' do
+    it '#email nil' do
+      bld email: nil
+      expect(@o.error_on :email).to include "can't be blank"
+    end
+
     it '#name_last nil' do
       bld name_last: nil
       expect(@o.error_on :name_last).to include "can't be blank"
@@ -9,6 +14,10 @@ describe Person do
   end
 
   context 'db constraints w' do
+    it '#email nil' do
+      expect_db_error { svf(bld email: nil) }
+    end
+
     it '#name_last nil' do
       expect_db_error { svf(bld name_last: nil) }
     end
