b22e79096daeafa55aaa2e2ee2c3db4d7c6b8c44
Gary Fleshman <gfleshman@newforge-tech.com>
2013-09-09 08:20:39 -0400

    SearchHelper pass results_count (vs instance variable)

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -4,8 +4,8 @@ module SearchHelper
     t('helpers.search_results.count', count: @results_count || 0)
   end
 
-  def last_search_page?(page, per_page)
-    page >= (@results_count.to_f / per_page).ceil
+  def last_search_page?(results_count, page, per_page)
+    page >= (results_count.to_f / per_page).ceil
   end
 
   def search_results_info(results_count, params = params)
@@ -22,13 +22,13 @@ module SearchHelper
       t('helpers.search_results.multiple_pages',
         entry_name: entry_name.capitalize,
         first: page_offset + 1,
-        last: last_search_page?(page, per_page) ? results_count : page_offset + per_page)
+        last: last_search_page?(results_count, page, per_page) ? results_count : page_offset + per_page)
     end.html_safe
   end
 
-  def search_pagination_nav
+  def search_pagination_nav(results_count, params = params)
     page = params[:page].to_i
-    last_page_p = last_search_page?(page, Search::RESULTS_PER_PAGE)
+    last_page_p = last_search_page?(results_count, page, Search::RESULTS_PER_PAGE)
     ary = []
     return ary if page == 1 && last_page_p
     ary << (page > 1 ? "<li>#{link_to 'First', url_for(params.merge page: 1)}</li>" : "<li class=disabled>#{link_to 'First', '#'}</li>").html_safe
--- spec/helpers/search_helper_spec.rb
+++ spec/helpers/search_helper_spec.rb
@@ -21,17 +21,97 @@ describe SearchHelper do
   end
 
   context '#search_results_info' do
-    before { @params_core = {action: 'index', controller: 'location_searches'} }
+    before { @params = {action: 'index', controller: 'location_searches'} }
     describe  do
       it 'w @results_count == 1' do
-        expect(search_results_info 1, @params_core.merge(page: 1)).to eql '1 location found'
+        expect(search_results_info 1, @params.merge(page: 1)).to eql '1 location found'
       end
       it 'w @results_count == 2' do
-        expect(search_results_info 2, @params_core.merge(page: 1)).to eql 'All 2 locations found'
+        expect(search_results_info 2, @params.merge(page: 1)).to eql 'All 2 locations found'
       end
       context 'w @results_count == 11' do
-        it { expect(search_results_info 11, @params_core.merge(page: 1)).to eql 'Locations 1 - 11' }
-        it { expect(search_results_info 11, @params_core.merge(page: 2)).to eql 'Locations 11 - 11' }
+        it { expect(search_results_info 11, @params.merge(page: 1)).to eql 'Locations 1 - 10' }
+        it { expect(search_results_info 11, @params.merge(page: 2)).to eql 'Locations 11 - 11' }
+      end
+    end
+  end
+
+  context '#search_pagination_nav' do
+    before { @params = {action: 'index', controller: 'location_searches'} }
+    describe  do
+      it 'w 1 results page' do
+        expect(search_pagination_nav 10, @params.merge(page: 1)).to eql []
+      end
+      context 'w 2 result pages' do
+        describe 'w page 1' do
+          subject { search_pagination_nav 11, @params.merge(page: 1) }
+          it do
+            expect(subject.length).to eql 2
+            expect(subject[0]).to match /<li class=disabled><a href="#">First<\/a><\/li>/
+            expect(subject[1]).to match /<li><a href="\/location_searches\?page=2">Next<\/a><\/li>/
+          end
+        end
+
+        describe 'w page 2' do
+          subject { search_pagination_nav 11, @params.merge(page: 2) }
+          it do
+            expect(subject.length).to eql 2
+            expect(subject[0]).to match /<li><a href="\/location_searches\?page=1">First<\/a><\/li>/
+            expect(subject[1]).to match /<li class=disabled><a href="#">Last<\/a><\/li>/
+          end
+        end
+      end
+
+      context 'w 3 result pages' do
+        describe 'w page 1' do
+          subject { search_pagination_nav 21, @params.merge(page: 1) }
+          it do
+            expect(subject.length).to eql 2
+            expect(subject[0]).to match /<li class=disabled><a href="#">First<\/a><\/li>/
+            expect(subject[1]).to match /<li><a href="\/location_searches\?page=2">Next<\/a><\/li>/
+          end
+        end
+
+        describe 'w page 2' do
+          subject { search_pagination_nav 21, @params.merge(page: 2) }
+          it do
+            expect(subject.length).to eql 2
+            expect(subject[0]).to match /<li><a href="\/location_searches\?page=1">First<\/a><\/li>/
+            expect(subject[1]).to match /<li><a href="\/location_searches\?page=3">Next<\/a><\/li>/
+          end
+        end
+
+        describe 'w page 3' do
+          subject { search_pagination_nav 21, @params.merge(page: 3) }
+          it do
+            expect(subject.length).to eql 3
+            expect(subject[0]).to match /<li><a href="\/location_searches\?page=1">First<\/a><\/li>/
+            expect(subject[1]).to match /<li><a href="\/location_searches\?page=2">Prev<\/a><\/li>/
+            expect(subject[2]).to match /<li class=disabled><a href="#">Last<\/a><\/li>/
+          end
+        end
+      end
+
+      context 'w 4 result pages' do
+        describe 'w page 3' do
+          subject { search_pagination_nav 31, @params.merge(page: 3) }
+          it do
+            expect(subject.length).to eql 3
+            expect(subject[0]).to match /<li><a href="\/location_searches\?page=1">First<\/a><\/li>/
+            expect(subject[1]).to match /<li><a href="\/location_searches\?page=2">Prev<\/a><\/li>/
+            expect(subject[2]).to match /<li><a href="\/location_searches\?page=4">Next<\/a><\/li>/
+          end
+        end
+
+        describe 'w page 4' do
+          subject { search_pagination_nav 31, @params.merge(page: 4) }
+          it do
+            expect(subject.length).to eql 3
+            expect(subject[0]).to match /<li><a href="\/location_searches\?page=1">First<\/a><\/li>/
+            expect(subject[1]).to match /<li><a href="\/location_searches\?page=3">Prev<\/a><\/li>/
+            expect(subject[2]).to match /<li class=disabled><a href="#">Last<\/a><\/li>/
+          end
+        end
       end
     end
   end
